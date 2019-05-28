23895809a5880d5e4a9bdc5cc545e29476572368
Katamaranas <ernestas.zidokas@gmail.com>
2019-05-28 21:28:43 +0300

    Sorted out bugs with duplicated select input, added several Jokes API for more randomn jokes to say

--- js/jspopup.js
+++ js/jspopup.js
@@ -1,73 +1,101 @@
 let synthesis = window.speechSynthesis;
 let voices = [];
-
-document.addEventListener('DOMContentLoaded', event => {
-  // createVoiceSelect();
-  if (speechSynthesis.onvoiceschanged !== undefined) {
-    speechSynthesis.onvoiceschanged = createVoiceSelect;
-  }
-});
+let baseContainer = document.querySelector('#base-container');
+let voiceSelect = document.querySelector('#voiceSelect');
+let previousVoice = '';
 
 document.querySelector('body').addEventListener('keypress', event => {
   if(event.keyCode === 13) {
-    console.log('enter');
     let text = document.querySelector('#text').value;
-    let selectedVoiceName = document.querySelector('#voiceSelect').value;
-    let selectedVoiceObject = voices.find(voice => voice.name == selectedVoiceName);
-
-    let utterance = new SpeechSynthesisUtterance(text);
-    utterance.voice = selectedVoiceObject;
-    utterance.pitch = document.querySelector('#pitchRange').value;
-    utterance.rate = document.querySelector('#rateRange').value;
-    synthesis.speak(utterance);
+    speak(text);
   }
+});
+
+document.querySelector('#voiceSelect').addEventListener('change', event => {
+  previousVoice = event.target.value;
 });
 
 document.querySelector('#buttons').addEventListener('click', event => {
   switch (event.target.id) {
     case 'pause':
-      synthesis.pause();
+    synthesis.pause();
     break;
     case 'resume':
-      synthesis.resume();
+    synthesis.resume();
     break;
     case 'cancel':
-      synthesis.cancel();
+    synthesis.cancel();
     break;
   }
 });
 
 document.querySelector('#joke').addEventListener('click', event => {
-  fetch('http://api.icndb.com/jokes/random')
-  .then(response => {
-    return response.json();
-  })
-  .then(result => {
-    let text = result.value.joke;
-    let selectedVoiceName = document.querySelector('#voiceSelect').value;
-    let selectedVoiceObject = voices.find(voice => voice.name == selectedVoiceName);
+  const jokesArray = ['http://api.icndb.com/jokes/random',
+  'https://official-joke-api.appspot.com/random_joke',
+  'https://icanhazdadjoke.com/'
+];
 
-    let utterance = new SpeechSynthesisUtterance(text);
-    utterance.voice = selectedVoiceObject;
-    utterance.pitch = document.querySelector('#pitchRange').value;
-    utterance.rate = document.querySelector('#rateRange').value;
-    synthesis.speak(utterance);
-  })
-  .catch(err => console.log(err));
+let rand = getRandomInt(3);
+let jokeText = '';
+
+fetch(jokesArray[rand], {
+  headers: { "Accept": "application/json" }
+})
+.then(response => {
+  return response.json();
+})
+.then(result => {
+
+  switch (rand) {
+    case 0:
+    jokeText = result.value.joke;
+    break;
+    case 1:
+    jokeText = result.setup + ' ' + result.punchline;
+    break;
+    case 2:
+    jokeText = result.joke;
+    break;
+    default:
+  }
+  console.log(jokeText);
+  speak(jokeText);
+})
+.catch(err => console.log(err));
 })
 
 let counter = 0;
 
 function createVoiceSelect() {
+  voiceSelect.innerHTML = '';
   console.log(counter++);
   voices = synthesis.getVoices();
-  let select = document.createElement('select');
-  select.id = 'voiceSelect';
   voices.forEach(voice => {
     let option = document.createElement('option');
     option.value = voice.name;
     option.textContent = voice.name;
-    select.appendChild(option);
+    if(voice.name == previousVoice) {
+      option.selected = true;
+    }
+    voiceSelect.appendChild(option);
   });
-  document.querySelector('#base-container').appendChild(select);
+  baseContainer.appendChild(voiceSelect);
+}
+
+createVoiceSelect();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = createVoiceSelect;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function speak(jokeText) {
+  let utterance = new SpeechSynthesisUtterance(jokeText);
+  let selectedVoiceObject = voices.find(voice => voice.name == voiceSelect.value);
+  utterance.voice = selectedVoiceObject;
+  utterance.pitch = document.querySelector('#pitchRange').value;
+  utterance.rate = document.querySelector('#rateRange').value;
+  synthesis.speak(utterance);
 }
